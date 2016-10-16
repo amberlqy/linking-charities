59adeae8f4a0ee7ffb8fd1add996dafcd89f3b23
Pat <mag_boy147128@hotmail.com>
2016-10-16 23:29:10 +0100

    front end code

---
+++ charities-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name linkingCharitiesWorldwideAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the linkingCharitiesWorldwideAppApp
+ */
+angular.module('linkingCharitiesWorldwideAppApp')
+  .controller('AboutCtrl', function () {
+
+  });
