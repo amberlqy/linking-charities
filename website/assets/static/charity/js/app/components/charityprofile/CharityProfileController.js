f488d7c671b64f91066b5614f8c7c608e92929a9
wpypeiyao <wpypeiyao@163.com>
2016-10-26 22:07:53 +0100

    add side-nav-bar and some functions

--- website/assets/static/charity/js/app/components/charityprofile/CharityProfileController.js
+++ website/assets/static/charity/js/app/components/charityprofile/CharityProfileController.js
@@ -1,4 +1,3 @@
-
 (function () {
     'use strict';
 
@@ -6,9 +5,9 @@
         .module('charity.charityprofile.controllers')
         .controller('CharityProfileController', CharityProfileController);
 
-    CharityProfileController.$inject = ['$scope', '$http'];
+    CharityProfileController.$inject = ['$scope', '$http', '$location', '$anchorScroll'];
 
-    function CharityProfileController($scope, $http) {
+    function CharityProfileController($scope, $http, $location, $anchorScroll) {
         charityProfile();
 
         function charityProfile() {
@@ -17,14 +16,37 @@
                     $scope.profileJson = response;
                 }
             );
+
+            $scope.gotoDescription = function () {
+                $location.hash('description');
+                $anchorScroll();
+            };
+            $scope.gotoFinancials = function () {
+                $location.hash('financials');
+                $anchorScroll();
+            };
+
+            $scope.gotoActivities = function () {
+                $location.hash('activities');
+                $anchorScroll();
+            };
+
+            $scope.gotoDocuments = function () {
+                $location.hash('documents');
+                $anchorScroll();
+            };
+
+
         }
 
-        $scope.urlLink = function getImageSrc(){
+        $scope.urlLink = function getImageSrc() {
             $http.get('').success(
                 function (response) {
                     $scope.urlLink = response;
                 }
             );
         }
+
+
     }
 })();
