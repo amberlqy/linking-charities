4d6c992be9ee9d7bcee81c4533f5d666656c0f56
Pat <mag_boy147128@hotmail.com>
2016-10-26 17:30:37 +0100

    Prototype of update profile function

--- website/assets/static/charity/js/app/components/profiles/ProfileService.js
+++ website/assets/static/charity/js/app/components/profiles/ProfileService.js
@@ -25,8 +25,41 @@
         }
 
         function update(profile) {
-            return $http.post('/api/auth/charity_profile/' + profile + '/' + $cookies.get('token') + '/');
+            //return $http.post('/api/auth/charity_profile/' + profile + '/');// + $cookies.get('token') + '/');
             //return $http.put('/auth/api/accounts/' + profile.username + '/', profile);
+            var parameter = JSON.stringify(
+                {
+                    "charity_name": "Test",
+                    "location": "London",
+                    "goal": "Cure",
+                    "address": "14 London",
+                    "phone_number": "0789456"
+                }
+            );
+
+            /*
+            var res = $http.post('/auth/api/charity_profile/', parameter);
+		    res.success(function(data, status, headers, config) {
+			    alert("Success: " + JSON.stringify({data: data}));
+
+                //$scope.message = data;
+		    });
+		    res.error(function(data, status, headers, config) {
+			    alert( "failure message: " + JSON.stringify({data: data}));
+		    });*/
+
+
+            return $http({
+                method: 'POST',
+                url: '/api/auth/charity_profile/',
+                //url: '/auth/api/charity_profile/',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                // Change profile to parameter if it doesn't work
+                data: profile//parameter//profile
+            })
         }
     }
 })();
