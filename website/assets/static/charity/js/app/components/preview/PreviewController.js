a764aef7d3c9c70a9b8f1b683ce9ebdde7dbaa90
wpypeiyao <wpypeiyao@163.com>
2016-11-17 22:43:01 +0000

    improve charity page & preview page

--- website/assets/static/charity/js/app/components/preview/PreviewController.js
+++ website/assets/static/charity/js/app/components/preview/PreviewController.js
@@ -5,15 +5,19 @@
         .module('charity.preview.controllers')
         .controller('PreviewProfileController', PreviewProfileController);
 
-    PreviewProfileController.$inject = ['$scope', '$http'];
+    PreviewProfileController.$inject = ['$scope', '$http','$modal'];
 
-    function PreviewProfileController($scope, $http) {
+    function PreviewProfileController($scope, $http,$modal) {
+
         <!--get data about activities details-->
         $http.get('/static/charity/resources/profile.json').success(
             function (response) {
                 $scope.activityJson = response;
             }
         );
+        // $scope.closemodal=function () {
+        //     $modal.close();
+        // }
 
     }
 })();
