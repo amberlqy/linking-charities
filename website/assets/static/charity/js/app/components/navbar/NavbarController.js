6979fb74995d3d5050b2fa25279a366f442d5351
Pat <mag_boy147128@hotmail.com>
2016-12-29 16:56:43 +0000

    Add advance search feature (Front-end side)

--- website/assets/static/charity/js/app/components/navbar/NavbarController.js
+++ website/assets/static/charity/js/app/components/navbar/NavbarController.js
@@ -6,10 +6,11 @@
         .module('charity.navbar.controllers')
         .controller('NavbarController', NavbarController);
 
-    NavbarController.$inject = ['$scope', '$http', 'Authentication', '$location', 'Search', '$modal'];
+    NavbarController.$inject = ['$scope', '$http', 'Authentication', '$location', 'Search'];
 
-    function NavbarController($scope, $http, Authentication, $location, Search, $modal) {
+    function NavbarController($scope, $http, Authentication, $location, Search) {
         var vm = this;
+        vm.search = {};
         var userDetails = Authentication.getAuthenticatedAccount();
         if (userDetails != undefined){
             vm.user = userDetails.username;
@@ -19,7 +20,7 @@
         vm.logout = logout;
         vm.isActive = isActive;
 
-        $scope.Charities = [];
+        vm.search.suggestion = [];
 
         function logout() {
             Authentication.logout();
@@ -29,44 +30,58 @@
             return viewLocation === $location.path();
         }
 
+        vm.searchClick = function() {
+            if (vm.search.searchKey == undefined){
+                vm.search.searchKey = "";
+            }
+            var searchKey = { name: vm.search.searchKey,
+                              filter: null,
+                              country: null,
+                              city: null,
+                              tag: null
+                            };
+            console.log(searchKey);
+            // Search.setSearchKey(searchKey);
+            // $location.path('/search/#=' + Math.random().toString(36).substring(7));
+        };
+
+        vm.advanceSearchClick = function() {
+            var searchKey = { name: vm.search.charityname,
+                              filter: vm.search.filter,
+                              country: vm.search.country,
+                              city: vm.search.city,
+                              tag: angular.element('#tagsinput').val()
+                            };
+            console.log(searchKey);
+            // Search.setSearchKey(searchKey);
+            // $location.path('/search/#=' + Math.random().toString(36).substring(7));
+        };
+
+        $scope.inputSelected = function (selected) {
+            vm.search.searchKey = selected;
+            if (selected != "" && selected != null && selected != undefined) {
+                updateSuggestion(selected);
+            }
+        };
+
+        $scope.afterSelected = function (selected) {
+            if (selected != undefined) {
+                vm.search.searchKey = selected.title;
+            }
+        };
+
         function updateSuggestion(selected){
             $http.get('/api/charity/charity_search/', {params:{"all": true}}).then(getSuccessFn, getErrorFn);
 
             function getSuccessFn(data, status, headers, config) {
                 var search = data.data["charity_profiles"];
-                $scope.Charities = search;
+                console.log(search);
+                vm.search.suggestion = search;
             }
 
             function getErrorFn(data, status, headers, config) {
                 console.error('Getting Search failed! ' + status);
             }
         }
-
-        $scope.searchClick = function() {
-            var searchKey = {name: $scope.searchKeyWord,
-                              target: null,
-                              country: null,
-                              city: null,
-                              ranking: null
-                            };
-            Search.setSearchKey(searchKey);
-            $location.path('/search/#=' + Math.random().toString(36).substring(7));
-        };
-
-        $scope.inputSelected = function (selected) {
-            $scope.searchKeyWord = selected;
-            updateSuggestion(selected);
-        };
-
-        $scope.afterSelected = function (selected) {
-            $scope.searchKeyWord = selected.title;
-        }
-
-        $scope.advancedSearchClick = function () {
-            var modalInstance = $modal.open({
-                templateUrl: '/static/charity/js/app/components/navbar/advanced-search-modal.html',
-                controller: 'AdvancedSearchController'
-            });
-        };
     }
 })();
