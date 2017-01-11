540bc1001fc418aa58f34c405594e0b142d0a9a5
Pat <mag_boy147128@hotmail.com>
2017-01-11 14:37:35 +0000

    Remove snackbar

--- website/assets/static/charity/js/app/module.js
+++ website/assets/static/charity/js/app/module.js
@@ -12,7 +12,6 @@ angular
             'charity.authentication',
             'charity.navbar',
             'charity.home',
-            'charity.snackbar',
             'charity.profiles',
             'charity.search',
             'charity.payment',
